b90fb8327bc6af5feec96fb3f16c34c436de91e5
Koudocko <tyler@pcquest.ca>
2023-11-13 11:59:10 -0500

    Changed to reference counted pointers. Added insert split an display split methods. Removed dir member (for now).

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,7 @@ use split::*;
 
 fn main(){
     let mut windows = vec![Split::default()];
+    windows[0].insert_split(Split::new(None, None, 50), Orientation::Vertical);
+    windows[0].insert_split(Split::new(None, None, 75), Orientation::Horizontal);
+    windows[0].display_splits();
 }
--- src/split.rs
+++ src/split.rs
@@ -1,29 +1,68 @@
-use std::boxed::Box;
+use std::rc::Rc;
 
+#[derive(PartialEq)]
 pub enum Orientation{
     Vertical,
     Horizontal,
 }
 
-type SplitChild = Option<Box<Split>>;
+type SplitChild = Option<Rc<Split>>;
 
 pub struct Split{
-    dir: Orientation,
     v_child: SplitChild,
     h_child: SplitChild,
     size: u8,
 }
 
 impl Split{
-    pub fn new(dir: Orientation, v_child: SplitChild, h_child: SplitChild, size: u8)-> Self{
-        Split{ dir, v_child, h_child, size }
+    pub fn new(v_child: SplitChild, h_child: SplitChild, size: u8)-> Self{
+        Split{ v_child, h_child, size }
+    }
+
+    pub fn display_splits(&self){
+        println!("{}", self.size);
+
+        let mut next_split = &self.v_child;
+        if next_split.is_some(){
+            next_split.as_ref().unwrap().display_splits();
+        }
+
+        next_split = &self.h_child;
+        if next_split.is_some(){
+            next_split.as_ref().unwrap().display_splits();
+        }
+    }
+
+    pub fn insert_split(&mut self, mut split: Split, pos: Orientation){
+        if pos == Orientation::Vertical{
+            if let Some(v_child) = &self.v_child{
+                let children = Rc::clone(v_child);
+
+                split.v_child = Some(children);
+                self.v_child = Some(Rc::new(split));
+            }
+            else{
+                self.v_child = Some(Rc::new(split));
+            }
+        }
+        else{
+            if let Some(h_child) = &self.h_child{
+                let children = Rc::clone(h_child);
+
+                split.h_child = Some(children);
+                self.h_child = Some(Rc::new(split));
+
+            }
+            else{
+                self.h_child = Some(Rc::new(split));
+            }
+        }
     }
 }
 
 impl Default for Split{
     fn default()-> Self{
         Split{ 
-            dir: Orientation::Vertical, 
             v_child: None,
             h_child: None,
             size: 100,
